de9b4575d462381b4672fd793be860a9e8ef7131
cotyadams <cotyadams1@gmail.com>
2024-02-29 16:43:15 -0500

    all tests passed

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -7,23 +7,23 @@ exports.checkAccountPayload = async (req, res, next) => {
   let error = { status: 400 }
   let { name, budget } = req.body;
   if (name === undefined || budget === undefined) { 
-    error.message = "name and budget are required"
-    next(error);
+    res.status(400).json({ message: "name and budget are required" })
+    next(error)
   } 
   else if (typeof name !== "string") { 
-    error.message = "name of account must be a string"
+     res.status(400).json({ message: "name of account must be a string" })
     next(error);
   }
   else if (name.trim().length < 3 || name.trim().length > 100) {
-    error.message = "name of account must be between 3 and 100" 
+    res.status(400).json({ message: "name of account must be between 3 and 100" })
     next(error);
   }
   else if (!Number(budget)) { 
-    error.message = "budget of account must be a number" 
+    res.status(400).json({ message: "budget of account must be a number" })
     next(error);
   }
   else if (Number(budget) < 0 || Number(budget) > 1000000) { 
-    error.message = "budget of account is too large or too small"
+    res.status(400).json({ message: "budget of account is too large or too small" })
     next(error);
   }
   next();
@@ -33,7 +33,7 @@ exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
   const list = await getAll();
   if (await list.find(account => account.name === req.body.name)) {
-    next({ status: 400, message: "that name is taken" })
+    res.status(400).json({ message: "that name is taken" })
   } else next();
 }
 
@@ -41,6 +41,6 @@ exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
   const account = await getById(req.params.id);
   if (!account) {
-    next({ status: 404, message: 'account not found' })
+    res.status(404).json({ message: 'account not found' })
   } else next();
 }
